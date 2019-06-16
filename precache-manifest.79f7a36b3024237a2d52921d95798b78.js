aaf8cee01a35d802ae4a80a011eb7d38f9753b7c
Tomaz Júnior <tomaz.jr@gmail.com>
2019-06-16 01:55:50 +0100

    Updates

---
+++ precache-manifest.79f7a36b3024237a2d52921d95798b78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec91fc8af70367c2427b1b146a3ed29",
+    "url": "/serverless-video-upload/index.html"
+  },
+  {
+    "revision": "affd83299733a7debfc5",
+    "url": "/serverless-video-upload/static/css/main.f493a3c7.chunk.css"
+  },
+  {
+    "revision": "7e0f80cf3507d782b255",
+    "url": "/serverless-video-upload/static/js/2.101384ad.chunk.js"
+  },
+  {
+    "revision": "affd83299733a7debfc5",
+    "url": "/serverless-video-upload/static/js/main.2716a184.chunk.js"
+  },
+  {
+    "revision": "a94e390d37ddbc763514",
+    "url": "/serverless-video-upload/static/js/runtime~main.28e88800.js"
+  }
+]);
